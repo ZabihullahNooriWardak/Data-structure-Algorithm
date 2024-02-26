db1bd02fbb81966b58cffa01e9c4c81b7301da2e
Zabih Noori <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-26 16:39:55 +0430

    direct commit to master branch

---
+++ practice.js
@@ -0,0 +1,68 @@
+class Node {
+    constructor (value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor () {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    peek() {
+        return this.top;
+    }
+    push(value) {
+        let node = new Node(value);
+        if (!this.bottom) {
+            this.bottom = node;
+            this.top = this.bottom;
+            this.length++;
+            return this.top;
+        }
+        let currentNode = this.bottom;
+        let counter = 1;
+        while (counter < this.length) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        currentNode.next = node;
+        this.top = currentNode.next;
+        this.length++;
+        return this.top;
+    }
+    pop() {
+        if (this.length === 0) {
+            return "nothing to pop";
+        }
+        if (this.length === 1) {
+            this.bottom = null;
+            this.top=this.bottom;
+            this.length--;
+            return;
+        }
+        let counter = 1;
+        let currentNode = this.bottom;
+        while (counter < this.length - 1) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        currentNode.next = null;
+        this.top = currentNode;
+        this.length--;
+        return currentNode;
+    }
+    isEmpty() {
+        if (this.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+let stack = new Stack();
+console.log(stack.push(1))
