74f992b61b5fd19371472fc0cc5af5bb3b437785
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-27 16:37:27 +0430

    implements basiq queue without checking alot

--- practice.js
+++ practice.js
@@ -1,28 +1,58 @@
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
+    constructor (value) {
+        this.value = value;
+        this.next = null;
     }
-  }
+}
 
-  class Queue {
-    constructor() {
-      this.first = null;
-      this.last = null;
-      this.length = 0;
+class Queue {
+    constructor () {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
     }
     peek() {
+        return this.first;
     }
     enqueue(value) {
+        let newNode = new Node(value)
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let pointerToFirst = this.first;
+            this.first = newNode;
+            this.first.next = pointerToFirst;
+            this.last = pointerToFirst;
+        }
+        this.length++;
     }
     dequeue() {
+        if (this.length === 0) {
+            return "nothing to dequeu";
+        }
+        if (this.length === 1) {
+            this.first = null;
+            this.last = this.first;
+            return;
+        }
+        let pointerToFirst = this.first;
+        this.first = pointerToFirst.next;
+        this.length--;
     }
-    //isEmpty;
-  }
+    isEmpty() {
+        this.length === 0 ? true : false;
+    }
+}
 
-  const myQueue = new Queue();
+const myQueue = new Queue();
 
-  //Joy
-  //Matt
-  //Pavel
-  //Samir
+myQueue.enqueue(" Joy");
+myQueue.enqueue("ali");
+myQueue.enqueue(" max");
+myQueue.dequeue();
+myQueue.dequeue();
+console.log(myQueue);
+//Matt
+//Pavel
+//Samir
