161c0cbc8abfb7061529fdce1c87b25cbca4cc3a
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-03-05 13:28:46 +0430

    fix small bug

--- practice.js
+++ practice.js
@@ -1,6 +1,9 @@
 // Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
 
 function findFactorialRecursive(number) {
+  if(number===0){
+    return 1;
+  }
  if(number===1){
  return number;
  }
