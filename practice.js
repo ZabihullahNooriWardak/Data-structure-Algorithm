9f4a3b7a75c6dadb28cf864b716b5370271f6a24
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-03-11 15:08:52 +0430

    adds teacher solution

--- practice.js
+++ practice.js
@@ -3,13 +3,27 @@ function reverseString(str) {
   for (let i = str.length - 1; i >= 0; i--) {
     newStr += str[i];
   }
-return newStr;
+  return newStr;
 }
 
 console.log(reverseString("yoyo master"));
 
 function reverseStringRecursive(str) {
-
-}
+  if (str === "") {
+    return "";
+  } else {
+    return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+  }
+}~
 
 reverseStringRecursive("yoyo master");
+
+
+
+
+
+
+
+
+
+
