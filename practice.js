83b5c1b5e8b09cafac173e19a163c9964e572693
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-03-13 15:38:56 +0430

    removes extra commits

--- practice.js
+++ practice.js
@@ -22,25 +22,24 @@ console.log(selectionSort(numbers));
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
-    const length = array.length;
+  const length = array.length;
 
-    for (let i = 0; i < length - 1; i++) {
-        // Set current index as minimum
-        let min = i;
-        let temp = array[i];
+  for (let i = 0; i < length - 1; i++) {
+    // Set current index as minimum
+    let min = i;
+    let temp = array[i];
 
-        for (let j = i + 1; j < length; j++) {
-            if (array[j] < array[min]) {
-                // Update minimum if current is lower that what we had previously
-                min = j;
-            }
-        }
-
-        array[i] = array[min];
-        array[min] = temp;
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
     }
 
-    return array;
+    array[i] = array[min];
+    array[min] = temp;
+  }
+
+  return array;
 }
 
 console.log(selectionSort(numbers));
