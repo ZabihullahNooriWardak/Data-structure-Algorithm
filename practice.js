0fae46caf1f26ea099bfc404f339c3afc092815e
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-17 09:31:23 +0430

    adds extra output for learning purpose

--- practice.js
+++ practice.js
@@ -6,15 +6,17 @@ function mergeSort(arr) {
     const middle = Math.floor(arr.length / 2);
     const left = arr.slice(0, middle);
     const right = arr.slice(middle);
-
+    console.log('left', left);
+    console.log('right', right);
     return merge(mergeSort(left), mergeSort(right));
 }
-
+console.log("nothing333333333333333333333333333333333333333333333333333333333333333333")
 function merge(left, right) {
     let result = [];
     let leftIndex = 0;
     let rightIndex = 0;
-
+console.log('leftttttttttttttttttttttt', left);
+console.log('righttttttttttttttttttttt', right);
     while (leftIndex < left.length && rightIndex < right.length) {
         if (left[leftIndex] < right[rightIndex]) {
             result.push(left[leftIndex]);
@@ -24,12 +26,12 @@ function merge(left, right) {
             rightIndex++;
         }
     }
-
+console.log("leftlast", left);
+console.log("rightlast", right);
+console.log("result", result);
+console.log("leftIndex", leftIndex);
+console.log("rightIndex", rightIndex);
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
-// Example usage:
-const array = [8, 3, 9, 5, 1, 4, 6, 2, 7];
-console.log("Original Array:", array);
-const sortedArray = mergeSort(array);
-console.log("Sorted Array:", sortedArray);
+console.log(mergeSort([4, 3, 2, 1, 5, 6, 7, 8, 9, 10]));
