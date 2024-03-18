782aaaa6f3f7f820d74aafbe91abed6f462d122a
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-18 07:11:46 +0430

    adds code on my own

--- practice.js
+++ practice.js
@@ -1,10 +1,19 @@
-function zab(){
-let myVar = 2;
-    function nas() {
-        myVar = 3;
-        console.log(myVar); // 3
-    }
-    console.log(myVar); // 2
+function quickSort(arr){
+if(arr.length<=1){
+    return arr;
 }
-zab();
-// nas(); // ReferenceError: nas is not defined
+let arrLeft=[];
+let arrRight=[];
+let pivot = arr[Math.floor(arr.length/2)];
+for(let i=0;i<arr.length;i++){
+  if(arr[i]<pivot ||(arr[i]===pivot && i !==Math.floor(arr.length/2) )){
+    arrRight.push(arr[i]);
+  }
+  if(arr[i]>pivot){
+    arrLeft.push(arr[i]);
+  }
+}
+return [...quickSort(arrRight),pivot,...quickSort(arrLeft)];
+}
+let myArray=[99,3,22,42,4,9,111,22];
+console.log(quickSort(myArray));
