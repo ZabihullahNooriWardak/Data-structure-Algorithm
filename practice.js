ff3459f57f21a5a7107d2479059f4d42e498de40
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-18 06:46:47 +0430

    js concept

--- practice.js
+++ practice.js
@@ -1,27 +1,10 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
+function zab(){
+let myVar = 2;
+    function nas() {
+        myVar = 3;
+        console.log(myVar); // 3
     }
-
-    const pivot = arr[Math.floor(arr.length / 2)];
-    const left = [];
-    const right = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (i === Math.floor(arr.length / 2)) {
-            continue;
-        }
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-
-    return [...quickSort(left), pivot, ...quickSort(right)];
+    console.log(myVar); // 2
 }
-
-// Example usage:
-const unsortedArray = [5, 3, 7, 2, 9, 1, 6, 4, 8];
-const sortedArray = quickSort(unsortedArray);
-console.log(sortedArray);
+zab();
+// nas(); // ReferenceError: nas is not defined
