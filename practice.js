d96fc7a161d09c47393d1c3af277b1965b395e1e
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-21 16:30:50 +0430

    initial commit

--- practice.js
+++ practice.js
@@ -1,3 +1,2 @@
-function practiceFunctionPassingAsParameterToFunction(callback) {
-  callback();
-}
+//binary search is algorithm used for searching in sorted array but bst is data structure
+// and here is the implmentation of binary search algorithm
