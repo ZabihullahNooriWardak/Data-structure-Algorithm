9a06f7923f71a932c1e74592c5fb8da151cec024
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-25 19:36:38 +0430

    without dynamic programming

---
+++ practice.js
@@ -0,0 +1,11 @@
+let calculations = 0;
+function fibonacci(n){
+  calculations++;
+  console.log(calculations);
+if(n<2){
+  return n;
+}
+return fibonacci(n-1)+fibonacci(n-2);
+}
+
+// 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610
