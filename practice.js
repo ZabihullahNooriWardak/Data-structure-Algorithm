0c45deadfc2eaca668b03426caed858bb748e05e
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-26 19:03:21 +0430

    Adds solution

--- practice.js
+++ practice.js
@@ -1,20 +1,35 @@
-function outerFunction() {
-  var outerVariable = 0;
-  
-  function innerFunction() {
-      outerVariable++;
-      console.log(outerVariable);
-  }
-  
-  return innerFunction;
-}
 
-var innerFunc1 = outerFunction();
-var innerFunc2 = outerFunction();
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
 
-innerFunc1(); // Output: 1
-innerFunc1(); // Output: 2
-innerFunc1(); // Output: 3
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 
-innerFunc2(); // Output: 1 (Independent from innerFunc1)
-innerFunc2(); // Output: 2
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+// Example 2:
+
+// Input: nums = [2,7,9,3,1]
+// Output: 12
+// Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+// Total amount you can rob = 2 + 9 + 1 = 12.
+
+var rob = function(nums) {
+  let oddTotal=0;
+  let evenTotal=0;
+    for(let i=0;i<nums.length;nums++){
+      if(i%2===0){
+        evenTotal+=nums[i];
+       }else{
+        oddTotal+=nums[i];
+       }
+    }
+    if(oddTotal>=evenTotal){
+      return oddTotal;
+    }
+    return evenTotal;
+};
