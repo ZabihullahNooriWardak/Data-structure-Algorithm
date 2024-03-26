ecb72682539d1e65c99811aa2851a57e779327ec
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-26 17:50:14 +0430

    nothing

--- practice.js
+++ practice.js
@@ -7,5 +7,13 @@ if(n<2){
 }
 return fibonacci(n-1)+fibonacci(n-2);
 }
-
+fibonacci(15);
 // 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610
+function fibonacciWithDynamicprogramming(n){
+  let fib = [0,1];
+  for(let i=2;i<=n;i++){
+    fib[i] = fib[i-1]+fib[i-2];
+  }
+  return fib[n];
+}
+
