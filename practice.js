8bc33b52f9e4d5487f6f63099955159fffe435e7
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-27 06:50:09 +0430

    solves the small bug

--- practice.js
+++ practice.js
@@ -1,22 +1,23 @@
 var rob = function(nums) {
+  let totalRob=0;
+  //[1,2,3,1]
+while(nums.length){
   let max = nums[0];
   let indexOfMax=0;
-  let totalRob=0;
-while(nums.length){
   for(let i=0;i<nums.length;i++){
     if(nums[i]>max){
      max=nums[i];
      indexOfMax=i;
     }
-    totalRob+=max;
+  }
+  totalRob+=max;
    if(indexOfMax>0){
-     nums.splice(indexOfMax-1,3)
+     nums.splice((indexOfMax-1),3)
    }else{
      nums.splice(indexOfMax,2)
    }
-  }
 }
 return totalRob;
 };
 
-console.log(rob([2,1,1,2]));
+console.log(rob([1,2,3,1]));
