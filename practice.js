d622dc6ba170055ada582d40713c5cd5228e8530
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-03-30 09:01:46 +0430

    adds the correct solution

--- practice.js
+++ practice.js
@@ -1,8 +1,28 @@
-
+//2,3,8,9,2,33,1
 var rob = function(nums) {
-  
+  let db=new Array(nums.length);
+  db[0]=nums[0];
+  db[1]=Math.max(nums[0],nums[1]);
+  for(let i=2;i<nums.length;i++){
+    db[i]=Math.max(db[i-2]+nums[i],db[i-1]);
+  }
+  return db[db.length-1];
 };
-console.log(rob([6,3,10,8,2,10,3,5,10,5,3]))
+console.log(rob([2,0,1,2]))
 
 //still fails in this test Case : [8,9,9,4,10,5,6,9,7,9]
 // now fails in this : [183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
